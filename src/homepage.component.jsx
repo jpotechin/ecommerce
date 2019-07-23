e78691a0308f34ab34b522e4734337569e8ac2f1
Joseph Potechin <josephpotechin@gmail.com>
2019-07-22 18:37:36 -0700

    Missing catagory
    Added missing catagory and fixed text

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -6,13 +6,19 @@ const HomePage = () => (
         <div className='directory-menu'>
             <div className='menu-item'>
                 <div className="content">
-                    <h1 className='title'>Hats</h1>
+                    <h1 className='title'>HATS</h1>
                     <span className='subtitle'>SHOP NOW</span>
                 </div>
             </div>
             <div className='menu-item'>
                 <div className="content">
-                    <h1 className='title'>Jackets</h1>
+                    <h1 className='title'>JACKETS</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className="content">
+                    <h1 className='title'>SNEAKERS</h1>
                     <span className='subtitle'>SHOP NOW</span>
                 </div>
             </div>
