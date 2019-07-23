f0c8afbd3ff34a5c0fe4e99077bba42eb2c97be3
Joseph Potechin <josephpotechin@gmail.com>
2019-07-22 18:35:42 -0700

    homepage setup/style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import './homepage.component';
+import HomePage from './homepage.component';
 
 function App() {
   return (
     <div>
-      Hello World!!
+      <HomePage />>
     </div>
+    
   );
 }
 
