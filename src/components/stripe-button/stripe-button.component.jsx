aef95dd14a60648c00f423738d3ef4db852197d8
Joseph Potechin <josephpotechin@gmail.com>
2019-07-31 08:23:17 -0700

    Updated stripe integration, favicon, title and site logo

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_JV4BK8i6jrabPAoTWCPgbHwd00d14n7hv3";
+
+  const onToken = token => {
+    console.log(token);
+    alert("Payment Succesful!");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="Authentic Regis Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
