19466ce0aeb7c5f30e72fe7b3b4e401fe285d17f
Joseph Potechin <josephpotechin@gmail.com>
2019-07-30 20:20:16 -0700

    Fixed an error in header link
    When on /shop/hat or /shop/jacket when clicking shop link it would navigated to shop/shop and create an error.

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -19,7 +19,7 @@ const Header = ({ currentUser, hidden }) => (
       <Logo className="logo" />
     </Link>
     <div className="options">
-      <Link className="option" to="shop">
+      <Link className="option" to="/shop">
         SHOP
       </Link>
       <Link className="option" to="shop">
